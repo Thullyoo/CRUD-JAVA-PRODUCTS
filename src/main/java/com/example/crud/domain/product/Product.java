0260ee3297b58bc9078cc4b032400d6fef11959b
Thullyoo <123775158+Thullyoo@users.noreply.github.com>
2024-02-25 15:08:07 -0300

    CRUD concluido

---
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,31 @@
+package com.example.crud.domain.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Table(name = "product")
+@Entity(name = "product")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer price_in_cents;
+
+    private Boolean active;
+
+    public Product(RequestProductDTO requestProductDTO){
+        this.name = requestProductDTO.name();
+        this.price_in_cents = requestProductDTO.price_in_cents();
+        this.active = true;
+    }
+}
